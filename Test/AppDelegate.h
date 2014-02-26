e8e1ac0c23d50be57f5b5f91c4aebd0a166b27f6
Shunmugaraj <shunmugaraj@ec.is>
2014-02-26 16:19:45 +0530

    First

---
+++ Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Shunmugaraj on 26/02/14.
+//  Copyright (c) 2014 ecsoftware.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Shunmugaraj on 26/02/14.
+//  Copyright (c) 2014 ecsoftware.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
